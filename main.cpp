748e54d43729c8b7de0f9cdddff13d3b7f4e93b9
unknown <agecev@gmail.com>
2026-05-30 17:33:08 +0300

    pradetas main failas

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using std::getline;
+using std::string;
+using std::map;
+
+void skt(){
+
+    std::ifstream F("text.txt");
+    string eil;
+
+    while (getline(F,eil)){
+        
+    }
+    
+}
+
+int main(){
+
+map<string, int> daznis;
+
+
+}
