b3c4d145dfe05fa0d2fb52064bc0f63b1bfd9d60
unknown <agecev@gmail.com>
2026-05-30 19:26:46 +0300

    stringstream

--- main.cpp
+++ main.cpp
@@ -2,18 +2,25 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <set>
+#include <sstream>
 
 using std::getline;
 using std::string;
 using std::map;
+using std::set;
 
-void skt(){
-
+void skt(map<string, int> daznis, map<string, set<int>> kur, set<string> url){
+    
     std::ifstream F("text.txt");
     string eil;
+    int eilsk=0;
 
     while (getline(F,eil)){
-        
+        eilsk++;
+
+
+        std::stringstream s(eil);
     }
     
 }
@@ -21,6 +28,7 @@ void skt(){
 int main(){
 
 map<string, int> daznis;
-
+map<string, set<int>> kur;
+set<string> url;
 
 }
